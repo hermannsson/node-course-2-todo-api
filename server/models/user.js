e21ee81096dd015b8363b88c789ff3d409058601
Ingvi Hermanns <ingvi.hermannsson@gmail.com>
2016-12-01 17:21:21 +0000

    Add test for POST /users/login

--- server/models/user.js
+++ server/models/user.js
@@ -72,12 +72,13 @@ UserSchema.statics.findByToken = function (token) {
 UserSchema.statics.findByCredentials = function (email, password) {
   var User = this;
 
-  User.findOne({email}).then((user) => {
+  return User.findOne({email}).then((user) => {
     if (!user) {
       return Promise.reject();
     }
 
     return new Promise((resolve, reject) => {
+      // Use bcrypt.compare to compare password and user.password
       bcrypt.compare(password, user.password, (err, res) => {
         if (res) {
           resolve(user);
